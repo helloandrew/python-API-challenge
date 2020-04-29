1091c80a431bf8fed8bdbd6c71a5cfd79c8436e5
helloandrew <leihouzhun@gmail.com>
2020-04-29 10:29:56 -0400

    Solution uploads

---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "63d9c417d92736b408ebb2dff08cc054"
+
+# Google API Key
+g_key = "AIzaSyBTpevi9Angg84Buffm6r1YyKBk09C8pQ4"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "d02515ef8944a850ae2b825b2b2e6d38"
+
+# Google API Key
+g_key = "AIzaSyBTpevi9Angg84Buffm6r1YyKBk09C8pQ4"
